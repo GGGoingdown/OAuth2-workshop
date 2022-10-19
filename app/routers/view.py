5021944dc4d44c8ef459419b385cc6a341fcf2fa
GGGoingdown <eddiepai0401@gmail.com>
2022-10-19 18:11:19 +0800

    Create notify index.html

--- app/routers/view.py
+++ app/routers/view.py
@@ -8,6 +8,15 @@ router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="app/templates")
 
 
-@router.get("/index")
-def canopy_dashboard(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@router.get("/login")
+async def login(request: Request):
+    response = templates.TemplateResponse("login.html", {"request": request})
+    response.delete_cookie(key="session_id")
+    return response
+
+
+@router.get("/notify")
+async def notify_page(request: Request):
+    response = templates.TemplateResponse("notify_index.html", {"request": request})
+    response.set_cookie(key="session_id", value="hello")
+    return response
